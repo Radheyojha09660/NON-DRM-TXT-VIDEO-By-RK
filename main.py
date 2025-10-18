f2cd7c92256aba42aa4a9fe868dc92496cd392bd
Radheyojha09660 <ojha.radhey1995@gmail.com>
2025-10-18 10:16:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -593,14 +593,18 @@ async def txt_handler(bot: Client, m: Message):
                     time.sleep(1)
 
             except Exception as e:
-                await bot.send_photo(
-    chat_id=m.chat.id,
-    photo=photologo,
-    caption=f"——— ✨ [{str(count).zfill(3)}]({link0}) ✨ ———\n\n📔𝐓𝐢𝐭𝐥𝐞 » `{name}`\n\n🔗𝐋𝐢𝐧𝐤 » <a href=\"{link0}\">__**Click Here to check manually**__</a>\n\n✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ `It's Rk`"
-)
-count += 1
-failed_count += 1
-continue
+    await bot.send_photo(
+        chat_id=m.chat.id,
+        photo=photologo,
+        caption=f"——— ✨ [{str(count).zfill(3)}]({link0}) ✨ ———\n\n"
+                f"📔𝐓𝐢𝐭𝐥𝐞 » `{name}`\n\n"
+                f"🔗𝐋𝐢𝐧𝐤 » <a href=\"{link0}\">__**Click Here to check manually**__</a>\n\n"
+                f"✦𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲 ✦ `It's Rk`"
+    )
+    count += 1
+    failed_count += 1
+    continue
+
 
     except Exception as e:
         await m.reply_text(e)
